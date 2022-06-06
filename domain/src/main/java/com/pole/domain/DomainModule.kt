9143a27901f31e0cd45e858383be5005e88abec5
Paolo D'Alessandro <98821286+paodbending@users.noreply.github.com>
2022-06-06 14:41:59 +0200

    Updating to match master (#1)
    * Added GetSearchResultsImpl
    
    * Updated androidx.core:core-ktx to 1.8.0, androidx.appcompat:appcompat to 1.4.2, com.google.android.material:material to 1.6.1
    
    * Updated GetSearchResults.kt to contain both interface and implementation and to be a fun interface

--- domain/src/main/java/com/pole/domain/DomainModule.kt
+++ domain/src/main/java/com/pole/domain/DomainModule.kt
@@ -1,5 +1,8 @@
 package com.pole.domain
 
+import com.pole.domain.usecases.GetSearchResults
+import com.pole.domain.usecases.GetSearchResultsImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -7,3 +10,11 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object DomainModule
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal interface DomainBindings {
+    @Binds
+    fun bindGetSearchResults(getSearchResultsImpl: GetSearchResultsImpl): GetSearchResults
+}
